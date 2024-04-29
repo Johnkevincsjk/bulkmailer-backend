c4763173ad3126c8d9b4ff9a0e8c2ac381011486
johnkevincsjk <johnkevin.csjk@gmail.com>
2024-04-29 23:13:19 +0530

    env added for port

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const PORT = 5000
 const cors = require('cors')
 app.use(cors())
-
+require('dotenv').config()
+const port = process.env.PORT
 
 
 app.use('/bulkmailer', require('./Routes/Routes'))
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log('Mail server is live')
 })
 
